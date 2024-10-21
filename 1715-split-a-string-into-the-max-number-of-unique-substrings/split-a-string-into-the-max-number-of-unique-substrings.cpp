8db52ed402c1daf71f9b38397a36cb12405dd4c4
Hung Nguyen <96320869+ndhung1104@users.noreply.github.com>
2024-10-21 14:44:27 +0700

    Time: 237 ms (57.31%) | Memory: 47.8 MB (81.58%) - LeetSync

---
+++ 1715-split-a-string-into-the-max-number-of-unique-substrings/split-a-string-into-the-max-number-of-unique-substrings.cpp
@@ -0,0 +1,33 @@
+class Solution {
+    unordered_set<string> substringSet;
+public:
+    void maxSplitRecursive(string s, int &max, int curr)
+    {
+        // if (s.size() + curr < max)
+        //     return -1;
+
+        if (s.size() == 0)
+            if (curr > max)
+                max = curr;
+        
+        for (int i = 1; i <= s.size(); i++)
+        {
+            string tmp = s.substr(0, i);
+            // cout << tmp << " " << s << " " << max << " " << curr << endl;
+            if (substringSet.find(tmp) == substringSet.end())
+            {
+                substringSet.insert(tmp);
+                maxSplitRecursive(s.substr(i), max, curr + 1);
+                substringSet.erase(substringSet.find(tmp));
+            }
+        }
+        
+    }
+    int maxUniqueSplit(string s) {
+        
+        int maxSplit = 1;
+        maxSplitRecursive(s, maxSplit, 0);
+
+        return maxSplit;
+    }
+};
