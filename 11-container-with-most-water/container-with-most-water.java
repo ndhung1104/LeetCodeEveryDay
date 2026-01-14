10bb57245dbdcd706f43ebf30d4789e1558b5329
Hung Nguyen <96320869+ndhung1104@users.noreply.github.com>
2026-01-14 09:21:36 +0700

    Time: 4 ms (93.09%) | Memory: 77.7 MB (5.54%) - LeetSync

--- 11-container-with-most-water/container-with-most-water.java
+++ 11-container-with-most-water/container-with-most-water.java
@@ -14,36 +14,14 @@ class Solution {
         int p1 = 0, p2 = height.length - 1;
         int ans = 0, container = Math.min(height[p1], height[p2]) * (p2 - p1);
         while (p1 < p2) {
-            // ans = Math.max(ans, container);
-            // if (height[p1] > height[p2]) {
-            //     container = height[p2] * (p2 - p1);
-            //     p2--;
-            // }
-            // else {
-            //     container = height[p1] * (p2 - p1);
-            //     p1++;
-            // }
-            int h1 = height[p1];
-            int h2 = height[p2];
-            
-            // Current "bottleneck" height
-            int currentMinHeight = Math.min(h1, h2);
-            
-            // Calculate area just once per "bottleneck"
-            ans = Math.max(ans, currentMinHeight * (p2 - p1));
-
-            // OPTIMIZATION:
-            // Instead of just if/else moving one step, we skip
-            // ALL lines that are not taller than the current bottleneck.
-            
-            // If p1 was the shorter (or equal) side, move it until we find a taller line
-            while (p1 < p2 && height[p1] <= currentMinHeight) {
-                p1++;
-            }
-            
-            // If p2 was the shorter (or equal) side, move it until we find a taller line
-            while (p1 < p2 && height[p2] <= currentMinHeight) {
+            ans = Math.max(ans, container);
+            if (height[p1] > height[p2]) {
+                container = height[p2] * (p2 - p1);
                 p2--;
+            }
+            else {
+                container = height[p1] * (p2 - p1);
+                p1++;
             }
         }
         ans = Math.max(ans, container);
